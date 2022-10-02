f512753abf50c747f6b4e476207b36726a3932d9
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-03 07:03:08 +0800

    AM 01st commit

---
+++ springboot_profile01/src/main/java/com/itheima/springboot_profile01/SpringbootProfile01Application.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_profile01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfile01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProfile01Application.class, args);
+    }
+
+}
---
+++ springboot_profile01/src/test/java/com/itheima/springboot_profile01/SpringbootProfile01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_profile01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfile01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
