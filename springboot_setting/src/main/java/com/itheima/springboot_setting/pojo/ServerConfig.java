1fe5399a15999fe2d796fc216f5ed25b3ec72938
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-05 08:05:09 +0800

    AM 01st commit

---
+++ springboot_setting/src/main/java/com/itheima/springboot_setting/pojo/ServerConfig.java
@@ -0,0 +1,24 @@
+package com.itheima.springboot_setting.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_basic_second
+ * BelongsPackage ==> com.itheima.springboot_setting.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-05 08:04:12
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class ServerConfig {
+
+    private String ipaddr;
+
+    private String port;
+
+    private Integer id;
+
+
+
+}
