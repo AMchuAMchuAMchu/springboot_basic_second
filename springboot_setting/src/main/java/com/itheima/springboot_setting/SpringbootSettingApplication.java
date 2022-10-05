6fefc9fc4f9e580bfa3ebb52a0d609ac435a8aef
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-05 08:03:32 +0800

    AM 01st commit

---
+++ springboot_setting/src/main/java/com/itheima/springboot_setting/SpringbootSettingApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSettingApplication.class, args);
+    }
+
+}
---
+++ springboot_setting/src/test/java/com/itheima/springboot_setting/SpringbootSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
