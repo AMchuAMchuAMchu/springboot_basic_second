3b0fae5efee22fb9dee2a70ebe5da2714ffbd805
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-05 08:54:46 +0800

    AM 01st commit

--- springboot_setting/src/main/java/com/itheima/springboot_setting/pojo/ServerConfig.java
+++ springboot_setting/src/main/java/com/itheima/springboot_setting/pojo/ServerConfig.java
@@ -2,10 +2,15 @@ package com.itheima.springboot_setting.pojo;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.convert.DurationFormat;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -20,18 +25,22 @@ import java.time.temporal.ChronoUnit;
 @Data
 @Component
 @ConfigurationProperties(prefix = "servers")
+@Validated
 public class ServerConfig {
 
     private String ipaddr;
 
 
 
-    private Duration port;
+    @Max(value = 9000,message = "死了ma吗???😂😂😂")
+    private Integer port;
 
 
     @DurationUnit(ChronoUnit.DAYS)
     private Duration timeout;
 
+    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize size;
 
 
 
