09ab4c8e80527af3b8927d290a661decd80e591e
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-06 08:40:32 +0800

    AM 01st commit

---
+++ springboot_test01/src/main/java/com/itheima/springboot_test01/pojo/AnimeInfo.java
@@ -0,0 +1,26 @@
+package com.itheima.springboot_test01.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_basic_second
+ * BelongsPackage ==> com.itheima.springboot_test01.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-06 08:39:37
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+    private String name;
+
+    private String character01;
+
+    private String character02;
+
+    private Integer releaseTime;
+
+    private Integer id;
+
+}
--- springboot_test01/src/test/java/com/itheima/springboot_test01/SpringbootTest01ApplicationTests.java
+++ springboot_test01/src/test/java/com/itheima/springboot_test01/SpringbootTest01ApplicationTests.java
@@ -4,14 +4,33 @@ import com.itheima.springboot_test01.test.AnimeName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 //@SpringBootTest(properties = {"test.prop=testValue02"},args = {"--test.prop=testValue03"})
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(AnimeName.class)
+@AutoConfigureWebMvc
 class SpringbootTest01ApplicationTests {
 
+
+    @Test
+    void testMockmvc(@Autowired MockMvc mockMvc) throws Exception {
+
+        MockHttpServletRequestBuilder msrb = MockMvcRequestBuilders.get("/animeInfo");
+
+        ResultActions perform = mockMvc.perform(msrb);
+
+        System.out.println(perform);
+
+
+    }
 
 
     @Value("${test.prop}")
