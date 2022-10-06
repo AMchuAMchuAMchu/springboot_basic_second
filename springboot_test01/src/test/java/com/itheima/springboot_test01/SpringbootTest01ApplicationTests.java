c0657db46c2b0af1a7f9202d4508614c3e5e6b39
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-06 08:31:17 +0800

    AM 01st commit

--- springboot_test01/src/test/java/com/itheima/springboot_test01/SpringbootTest01ApplicationTests.java
+++ springboot_test01/src/test/java/com/itheima/springboot_test01/SpringbootTest01ApplicationTests.java
@@ -3,17 +3,33 @@ package com.itheima.springboot_test01;
 import com.itheima.springboot_test01.test.AnimeName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+//@SpringBootTest(properties = {"test.prop=testValue02"},args = {"--test.prop=testValue03"})
 @SpringBootTest
+@Import(AnimeName.class)
 class SpringbootTest01ApplicationTests {
 
 
 
+    @Value("${test.prop}")
+    private String testValue01;
 
     @Autowired
     private String animeName;
+
+
+
+
+    @Test
+    void testPropValue(){
+
+        System.out.println("testValue::"+testValue01);
+
+    }
+
 
     @Test
     void contextLoads() {
--- springboot_test01/src/test/java/com/itheima/springboot_test01/test/AnimeName.java
+++ springboot_test01/src/test/java/com/itheima/springboot_test01/test/AnimeName.java
@@ -1,6 +1,7 @@
 package com.itheima.springboot_test01.test;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
  * CreateTime ==> 2022-10-06 08:14:00
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
-//@Component
+@Configuration
 public class AnimeName {
 
     @Bean
