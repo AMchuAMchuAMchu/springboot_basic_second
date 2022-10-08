2577cfdb96f68487f4c3fea3f39751d5264f1e46
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-09 06:52:23 +0800

    AM 01st commit

---
+++ springboot_transition01/springboot_transition01_dao/src/main/java/com/itheima/springboot_transition01_dao/SpringbootTransition01DaoApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_transition01_dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransition01DaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTransition01DaoApplication.class, args);
+    }
+
+}
---
+++ springboot_transition01/springboot_transition01_dao/src/test/java/com/itheima/springboot_transition01_dao/SpringbootTransition01DaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_transition01_dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransition01DaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
