f2c3915aeb7da9368c10f59bc37e03de4e00e870
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-09 15:07:59 +0800

    AM 01st commit

--- springboot_redis01/src/test/java/com/itheima/springboot_redis01/SpringbootRedis01ApplicationTests.java
+++ springboot_redis01/src/test/java/com/itheima/springboot_redis01/SpringbootRedis01ApplicationTests.java
@@ -3,14 +3,33 @@ package com.itheima.springboot_redis01;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringbootRedis01ApplicationTests {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
+
+
+	@Test
+	void testList(){
+
+		ListOperations lo = redisTemplate.opsForList();
+
+		lo.leftPushAll("name02","刀剑神域Alicization","尤吉欧","kirito","Alici");
+
+
+		List name02 = lo.range("name02", 0, -1);
+
+		name02.forEach(System.out::println);
+
+
+	}
 
 
 	@Test
