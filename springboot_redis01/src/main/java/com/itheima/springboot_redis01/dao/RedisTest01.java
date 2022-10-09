14d34dfbb8826e20420716bcdb1fa46f33a29865
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-09 14:30:05 +0800

    AM 01st commit

---
+++ springboot_redis01/src/main/java/com/itheima/springboot_redis01/dao/RedisTest01.java
@@ -0,0 +1,42 @@
+package com.itheima.springboot_redis01.dao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_basic_second
+ * BelongsPackage ==> com.itheima.springboot_redis01.dao
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-09 14:24:20
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Service
+public class RedisTest01 {
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @Test
+    public void testValue(){
+
+        ValueOperations vp = redisTemplate.opsForValue();
+
+        vp.set("name01","影宅...");
+
+        Object name01 = vp.get("name01");
+
+        System.out.println(" >> "+name01);
+
+    }
+
+
+
+
+
+}
